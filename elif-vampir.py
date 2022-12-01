3f038c0dc43492000a2039b30955e7e06e42dc47
victor-schumann <schumann@tutamail.com>
2022-12-01 23:40:13 +0000

    add rpg and manage files

---
+++ elif-vampir.py
@@ -0,0 +1,118 @@
+victory = ('''
+       _      _                   
+      (_)    | |                  
+__   ___  ___| |_ ___  _ __ _   _ 
+\ \ / / |/ __| __/ _ \| '__| | | |
+ \ V /| | (__| || (_) | |  | |_| |
+  \_/ |_|\___|\__\___/|_|   \__, |
+                             __/ |
+                            |___/ 
+''')
+game_over =('''
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
+██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
+███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
+██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
+███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼████▄┼┼┼▄▄▄▄▄▄▄┼┼┼▄████┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼▀▀█▄█████████▄█▀▀┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼┼█████████████┼┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼┼██▀▀▀███▀▀▀██┼┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼┼██┼┼┼███┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼┼█████▀▄▀█████┼┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼┼┼███████████┼┼┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼▄▄▄██┼┼█▀█▀█┼┼██▄▄▄┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼▀▀██┼┼┼┼┼┼┼┼┼┼┼██▀▀┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+''')
+
+print('''
+               ~"""~
+             /,/,/,/\\\ __
+           /,/,/,/,,\\`\ Y#b.
+          /,/,       \\\\D###D.
+        d#\_/   \_/\####D
+       d##\/,   | |    /,/####P
+      d###\,   (. .)  ,_/####P
+     d######\  ,v"v.  ,#####P
+    d########\ \"""/ / ###P"
+     d########\_____/ \##P
+ #b   T########\      /##b
+ ###b__)########`---=/###########b
+ ############ ||    /#############b
+ #######P"    ||   // .###########H
+ ####P        ||  //    T#########H
+ ####         || //       T#######H
+ ####         ||//          T#####H
+ ####         | /             T###H
+ ####         |/                T#H
+''')
+print("\nWELCOME TO...")
+print('''
+                                                      88             
+                                                      ""             
+                                                                     
+8b       d8 ,adPPYYba, 88,dPYba,,adPYba,  8b,dPPYba,  88 8b,dPPYba,  
+`8b     d8' ""     `Y8 88P'   "88"    "8a 88P'    "8a 88 88P'   "Y8  
+ `8b   d8'  ,adPPPPP88 88      88      88 88       d8 88 88          
+  `8b,d8'   88,    ,88 88      88      88 88b,   ,a8" 88 88          
+    "8"     `"8bbdP"Y8 88      88      88 88`YbbdP"'  88 88          
+                                          88                         
+                                          88                         
+''')
+
+#begin"
+print("\n\tYOUR MISSION IS TO ESCAPE!")
+print("--------------- LVL 01 ---------------\n")
+
+print("\tYou wake up in what seems to be a bedroom cell, which looks unlocked. As you peek outside you see a long corridor that goes left and right.\n\n\tOn the left you see in the distance some flickering lights and hears the sound of light fire. On the right you see nothing but darkness and you can hear the sounds of metalic sharp objects banging.\n")
+
+choice1 = input("\tDo you want to go LEFT or RIGHT? (L/R): ")
+if choice1.lower()=="l" or choice1.lower()=="left":
+    print("\n--------------- LVL 2 ---------------\n")
+    print("\tSo far, so good. You reach a large room with a fireplace, with a modest amount of logs burning. Ahead of you you are two exits. One of them goes downwards towards what seems to be a dark and quiet dungeon, and the other upwards to the tower, from where light is coming from.\n")
+    choice2 = input("\tDo you want to go UP or DOWN? (U/D): ")
+
+    if choice2.lower() == "u" or choice2.lower() == "up":
+        #tower level
+        print("\n--------------- LVL 3 ---------------\n")
+        print("You get to a tower on the left isle of the castle. The room is big and empty, except for a large wooden structure with a candle on it, perhaps a table, and no chair. You go towards the window and see a structure very similar to a stair, that could get you down to safety. But there seems to be some writing on the count's table that get your curiosity...")
+        choice3 = input("\nDo you want to CHECK the table or go towards STAIR? (C/S): ")
+        if choice3.lower() == 's' or choice3.lower == "stair":
+            print(f"{victory}")
+            print("\nCONGRATULATIONS, YOU MANAGED TO GET AWAY ALIVE!!\n")
+        else:
+            #death_2
+            print(f"{game_over}")
+            print("As you turn around and approach the wooden structure, you noticed it is not a table, but a coffin. Before you can properly react, it's seal is open with a bang, and the count himself turns to you. As he slowly extinguishes the candle, you get a glimpse of his red bright eyes.\n\nYour future seems short and painful.\n")
+    elif choice2.lower() == "d":
+        #death_3
+        print(f"{game_over}")
+        print("You go downwards through many stairs, to what seems to be a dark dinner room. You can barely see anything, so you grab a lighter and light it on. You see 10 faces with sharp teeth around the table, and one of them say to the others: 'Dinner arrived early, gentleman'. Your future seems short and painful.")
+    else:
+        #death_2
+        print(f"{game_over}")
+        print("You took too long to take your decision, and a vampire managed to get your scent! He bites you until you fall unconscius, slowly going towards your death.")
+        
+        
+  
+
+elif choice1.lower()=="r" or choice1.lower()=="right": 
+    #death_1
+    print(f"{game_over}")
+    print("\nI'm afraid you have discovered death in a most painful way. The vampire swordfighters of Dracula were having a sparring match, and you fit just right as a dummy for them to practice on.\n")
+
+else:
+    #death_2
+    print(f"{game_over}")
+    print("You took too long to take your decision, and a vampire managed to get your scent! He bites you until you fall, and right before you get unconscius, you feel yourself getting dragged towards a dungeon. Your future is dark and short.")
