3fc5123aebff44cdf3fda609b6597c3fe4e7a0f0
victor-schumann <schumann@tutamail.com>
2022-12-07 21:01:05 +0000

    while (failure): eat chocolate

--- day8_caesar.py
+++ day8_caesar.py
@@ -1,4 +1,7 @@
 # --- functions ---
+
+#Encrypt
+    #Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(text, shift, alphabet):
     caesar = []
     message = [x for x in text]
@@ -8,45 +11,59 @@ def encrypt(text, shift, alphabet):
         
         elif letter in alphabet:
             index = alphabet.index(letter)
+            ##🐛Bug alert: What happens if you try to encode 'z'?
             if index+shift < 26:
                 caesar.append(alphabet[index+shift])
             else:
                 caesar.append(alphabet[index+shift-26])
     return caesar
-    
+
+def decrypt(text, shift, alphabet):
+    caesar = []
+    message = [x for x in text]
+    for letter in message:
+        if letter == ' ':
+            caesar.append(' ')
+        
+        elif letter in alphabet:
+            index = alphabet.index(letter)
+            ##🐛Bug alert: What happens if you try to encode 'z'?
+            if index+shift < 26:
+                caesar.append(alphabet[index+shift])
+            else:
+                caesar.append(alphabet[index+shift-26])
+    return caesar
+
 # --- var declaration ---
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # --- code ---
-#direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-print(f"\n{text}\n")
-shift = int(input("Type the shift number:\n"))
+direction = ''
+ask = input("\nType 'E' to encrypt, type 'D' to decrypt (E/D): ").lower()
+while direction == '':
+    if ask == 'e':
+        direction = 'e'
+        text = input("\nType your message: ").lower()
+        shift = int(input("\nType the shift number: "))
 
-message = ''.join(encrypt(text, shift, alphabet))
-print(f"\n{message}\n")
+        message = ''.join(encrypt(text, shift, alphabet))
+        print(f"\nThe ENCODED message is: \n\n\t{message.upper()}\n")
 
+    elif ask == 'd':
+        direction = 'd'
+        text = input("\nType your message: ").lower()
+        shift = int(input("\nType the shift number: "))
 
-# for a in range(len(message)):
-#     if message[a] in alphabet:
-#         print(a)
-# print(f"\n{message}\n")
+        message = ''.join(decrypt(text, shift, alphabet))
+        print(f"The DECODED message is: \n\n\t{message.upper()}\n")
 
-#TODO-1: 
-    #Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+    else:
+        print("\nThat is not a valid option. Would you like to try again?")
+        direction = input("\nType 'E' to encrypt, type 'D' to decrypt (E/D): ").lower()
 
+#FAQ:
+    #https://stackoverflow.com/questions/5618878/how-to-convert-list-to-string
 
-#TODO-2: 
-    #Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+    #https://stackoverflow.com/questions/5453026/string-to-list-in-python
 
-    ##HINT: How do you get the index of an item in a list:
     #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
