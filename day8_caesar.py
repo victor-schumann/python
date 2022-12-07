cfa7150a767547bc81bd77eb0ed81441111c2c3d
victor-schumann <schumann@tutamail.com>
2022-12-07 21:29:05 +0000

    Fix while loop and optimize code

--- day8_caesar.py
+++ day8_caesar.py
@@ -3,49 +3,58 @@
 #Encrypt
     #Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(text, shift, alphabet):
-    caesar = []
-    message = [x for x in text]
-    for letter in message:
+    caesar = ''
+    #message = [x for x in text]
+    for letter in text:
         if letter == ' ':
-            caesar.append(' ')
+            caesar+=letter
         
         elif letter in alphabet:
-            index = alphabet.index(letter)
-            ##🐛Bug alert: What happens if you try to encode 'z'?
-            if index+shift < 26:
-                caesar.append(alphabet[index+shift])
+            position = alphabet.index(letter)
+            new_position = position+shift
+
+            if position+shift < 26:
+                new_letter = alphabet[new_position]
+                caesar+=new_letter
+
             else:
-                caesar.append(alphabet[index+shift-26])
+                new_letter = alphabet[new_position-26]
+                caesar+=new_letter
+
     return caesar
 
 def decrypt(text, shift, alphabet):
-    caesar = []
-    message = [x for x in text]
-    for letter in message:
+    caesar = ''
+    for letter in text:
         if letter == ' ':
-            caesar.append(' ')
+            caesar+=letter
         
         elif letter in alphabet:
-            index = alphabet.index(letter)
-            ##🐛Bug alert: What happens if you try to encode 'z'?
-            if index+shift < 26:
-                caesar.append(alphabet[index+shift])
+            position = alphabet.index(letter)
+            new_position = position-mjshift
+
+            if position+shift < 26:
+                new_letter = alphabet[new_position]
+                caesar+=new_letter
+
             else:
-                caesar.append(alphabet[index+shift-26])
+                new_letter = alphabet[new_position-26]
+                caesar+=new_letter
+                
     return caesar
 
 # --- var declaration ---
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # --- code ---
-direction = ''
-ask = input("\nType 'E' to encrypt, type 'D' to decrypt (E/D): ").lower()
-while direction == '':
+controlo = 0
+while controlo == 0:
+    ask = input("\nType 'E' to encrypt, type 'D' to decrypt (E/D): ").lower()
+    
     if ask == 'e':
-        direction = 'e'
         text = input("\nType your message: ").lower()
         shift = int(input("\nType the shift number: "))
-
+        controlo+=1
         message = ''.join(encrypt(text, shift, alphabet))
         print(f"\nThe ENCODED message is: \n\n\t{message.upper()}\n")
 
@@ -53,13 +62,14 @@ while direction == '':
         direction = 'd'
         text = input("\nType your message: ").lower()
         shift = int(input("\nType the shift number: "))
-
+        controlo+=1
         message = ''.join(decrypt(text, shift, alphabet))
-        print(f"The DECODED message is: \n\n\t{message.upper()}\n")
+        print(f"\nThe DECODED message is: \n\n\t{message.upper()}\n")
 
     else:
+        controlo=0
         print("\nThat is not a valid option. Would you like to try again?")
-        direction = input("\nType 'E' to encrypt, type 'D' to decrypt (E/D): ").lower()
+        # ask = input("\nType 'E' to encrypt, type 'D' to decrypt (E/D): ").lower()
 
 #FAQ:
     #https://stackoverflow.com/questions/5618878/how-to-convert-list-to-string
